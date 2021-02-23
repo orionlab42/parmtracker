fc42026d4fa021d0484dc3dabfc5e37f78049cf3
annakallo <anna.kallo@gmail.com>
2021-02-23 13:05:26 +0100

    Basic server setup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/annakallo/parmtracker
+
+go 1.15
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+	r := mux.NewRouter()
+
+	// for static pages like home
+	fs := http.StripPrefix("/home/", http.FileServer(http.Dir("./stat/")))
+	//http.Handle("/home/", fs)
+	r.PathPrefix("/home/").Handler(fs).Methods("GET")
+
+	// for dynamic routing
+	r.HandleFunc("/expenses", handler)
+	//http.HandleFunc("/hello", handler)
+	http.ListenAndServe(":80", r)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	fmt.Fprintf(w, "What did you spend?")
 }
