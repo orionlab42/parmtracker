f005998b2998447532d946d07d5c560958b4777d
annakallo <anna.kallo@gmail.com>
2022-03-30 19:55:38 +0200

    Fixed home add entry button to appear only when logged in

--- server/client/src/components/home.jsx
+++ server/client/src/components/home.jsx
@@ -6,15 +6,19 @@ const Home = (props) => {
     if (props.user === "") {
         menu =  ( <h3 className="title">{'You are not logged in' }</h3>)
     } else {
-        menu = (<h3 className="title">{'Hello ' + props.user.user_name}</h3>)
+        menu = (    <div>
+                        <h3 className="title">{'Hello ' + props.user.user_name}</h3>
+                        <div className="add-more">
+                            <Link to="/expenses/new" className="button is-link is-medium add-more-button">+</Link>
+                        </div>
+                     </div>)
+
     }
     return (
         <div>
             <h1 className="title">Home</h1>
             {menu}
-            <div className="add-more">
-                <Link to="/expenses/new" className="button is-link is-medium add-more-button">+</Link>
-            </div>
+
         </div>
     );
 };
